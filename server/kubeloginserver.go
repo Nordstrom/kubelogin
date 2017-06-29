b7846d11be810c111260f637d6a3577e593c7651
Carlton, Adam <Adam.Carlton@nordstrom.com>
2017-06-29 15:50:37 -0700

    current skeleton login server code. test file has been updated to cover this code

--- server/kubeloginserver.go
+++ server/kubeloginserver.go
@@ -4,7 +4,59 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
+
+func cliPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	mappedItems := r.PostForm
+	id := strings.Join(mappedItems["clientID"], "")
+	port := strings.Join(mappedItems["portNum"], "")
+	if id == "" || port == "" {
+		fmt.Fprint(w, "400 Bad request")
+		//may be better to do a log.Fatal() for this error
+		return
+	}
+	//will need to verify the Id based on some predetermined location that it's saved in. this will determine if it proceeds or returns a 404
+	realID := verifyID(id)
+	if !realID {
+		fmt.Fprint(w, "404 Not found")
+		return
+	}
+	idport := (id + "," + port)
+	//current way of proving that we can get the id and the port num to the server
+	fmt.Fprint(w, idport)
+}
+
+func authPostHandler(w http.ResponseWriter, r *http.Request) {
+	//need infor for auth server
+	fmt.Fprint(w, "good news everyone")
+}
+
+func authPostJwtHandler(w http.ResponseWriter, r *http.Request) {
+	//need info for auth server
+	fmt.Fprint(w, "good news everyone")
+}
+
+func postToAuthHandler(id string, secret string, authCode string) error {
+	//need info for auth server
+	fmt.Print("send to the auth server")
+	return nil
+}
+
+func postTokenToCliHandler(jwtToken string) error {
+	//will be easier to implement with a base cli created to communicate with this server
+	fmt.Print("send back to the client")
+	return nil
+}
+
+func verifyID(id string) bool {
+	//this will eventually grab the id from wherever it's stored and return true or false if it's valid or not
+	if id == "myclient" {
+		return true
+	}
+	return false
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	/*
--- server/kubeloginserver_test.go
+++ server/kubeloginserver_test.go
@@ -12,31 +12,88 @@ import (
 
 func TestSpecs(t *testing.T) {
 	Convey("Kubelogin Server Tests", t, func() {
-		testServer := httptest.NewServer(http.HandlerFunc(handler))
-		response, _ := http.Get(testServer.URL)
+		responseTestServer := httptest.NewServer(http.HandlerFunc(handler))
+		cliPostTestServer := httptest.NewServer(http.HandlerFunc(cliPostHandler))
+		authPostTestServer := httptest.NewServer(http.HandlerFunc(authPostHandler))
+		authPostJwtTestServer := httptest.NewServer(http.HandlerFunc(authPostJwtHandler))
 		Convey("The server should get a 200 response", func() {
+			response, _ := http.Get(responseTestServer.URL)
 			So(response.StatusCode, ShouldEqual, 200)
 		})
-		Convey("The postHandler shouldn't be able to know who the source is", func() {
-			resp, _ := http.Post(testServer.URL, "application/x-www-form-urlencoded", strings.NewReader("hello world"))
+		Convey("The cliPostHandler should receive clientID and port from the CLI", func() {
+			values := url.Values{}
+			values.Set("clientID", "myclient")
+			values.Set("portNum", "8000")
+			encode := values.Encode()
+			resp, _ := http.Post(cliPostTestServer.URL, "application/x-www-form-urlencoded", strings.NewReader(encode))
 			b, _ := ioutil.ReadAll(resp.Body)
-			So(string(b), ShouldEqual, "Unkown Post source :( \n")
+			result := string(b)
+
+			So(result, ShouldEqual, "myclient,8000")
 		})
-		Convey("The postHandler should know that the source is a client", func() {
-			v := url.Values{}
-			v.Set("post_from", "client")
-			s := v.Encode()
-			req, _ := http.NewRequest("POST", testServer.URL, strings.NewReader(s))
-			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-			c := &http.Client{}
-			resp, _ := c.Do(req)
-			data, _ := ioutil.ReadAll(resp.Body)
-			So(string(data), ShouldEqual, "Post from client! r.PostForm = map[post_from:[client]]\n")
+		Convey("If the ID or port are missing the cliPostHandler should return a 400 error", func() {
+			values := url.Values{}
+			values.Set("clientID", "myclient")
+			values.Set("portNum", "")
+			encode := values.Encode()
+			resp, _ := http.Post(cliPostTestServer.URL, "application/x-www-form-urlencoded", strings.NewReader(encode))
+			b, _ := ioutil.ReadAll(resp.Body)
+			result := string(b)
+
+			So(result, ShouldEqual, "400 Bad request")
 		})
-		Convey("The get request should be hoopla", func() {
+		Convey("Once the clientID is received, should verify if it exists", func() {
+			response := verifyID("myclient")
+			So(response, ShouldEqual, true)
+		})
+		Convey("If the clientID does not exist, return 404 error", func() {
+			response := verifyID("notmyclient")
+			var err string
+			if !response {
+				err = "404 Not found"
+			}
+			So(err, ShouldEqual, "404 Not found")
+		})
+		/*Convey("The server should keep a map of clientID's to secrets for later validation", func() {
 			response, _ := http.Get(testServer.URL)
 			body, _ := ioutil.ReadAll(response.Body)
 			So(string(body), ShouldEqual, "hoopla")
+		})
+		Convey("The server should keep a map of http session id's/local ports to confirm things are getting sent to right location", func() {
+			response, _ := http.Get(testServer.URL)
+			body, _ := ioutil.ReadAll(response.Body)
+			So(string(body), ShouldEqual, "hoopla")
+		})*/
+		Convey("Server should listen for callback from the authZ server (assuming a POST)", func() {
+			values := url.Values{}
+			values.Set("authCode", "myauthcode")
+			encode := values.Encode()
+			resp, _ := http.Post(authPostTestServer.URL, "application/x-www-form-urlencoded", strings.NewReader(encode))
+			b, _ := ioutil.ReadAll(resp.Body)
+			result := string(b)
+			So(result, ShouldEqual, "good news everyone")
+		})
+		/*Convey("Server should receive the auth code from auth server POST", func() {
+			response, _ := http.Get(testServer.URL)
+			body, _ := ioutil.ReadAll(response.Body)
+			So(string(body), ShouldEqual, "hoopla")
+		})*/
+		Convey("Server should make a POST request to authZ server containing clientID, clientSecret and Authcode", func() {
+			response := postToAuthHandler("id", "secret", "authCode")
+			So(response, ShouldEqual, nil)
+		})
+		Convey("Server should listen for the response by the authZ server containing the JWT", func() {
+			values := url.Values{}
+			values.Set("jwtToken", "myjwttoken")
+			encode := values.Encode()
+			resp, _ := http.Post(authPostJwtTestServer.URL, "application/x-www-form-urlencoded", strings.NewReader(encode))
+			b, _ := ioutil.ReadAll(resp.Body)
+			result := string(b)
+			So(result, ShouldEqual, "good news everyone")
+		})
+		Convey("Server should finally redirect JWT to CLI at CLI's local port", func() {
+			response := postTokenToCliHandler("jwtToken")
+			So(response, ShouldEqual, nil)
 		})
 	})
 }
