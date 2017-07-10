4778732e543243df67e5f29f3faf4aedf1d34142
adamcarlton <acarlton@uoregon.edu>
2017-07-10 16:58:39 -0700

    updated unit tests, closer to finished server code

--- server/kubeloginserver.go
+++ server/kubeloginserver.go
@@ -13,7 +13,9 @@ import (
 	"golang.org/x/oauth2"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
 
 type serverApp struct {
@@ -47,6 +49,26 @@ func (app *serverApp) handleCliLogin(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, authCodeURL, http.StatusSeeOther)
 }
 
+func getField(r *http.Request, fieldName string) string {
+	if r.FormValue(fieldName) != "" {
+		log.Print("fieldname: [" + r.FormValue(fieldName) + "]")
+		return r.FormValue(fieldName)
+	}
+	return ""
+}
+
+func jwtToString(claims json.RawMessage, w http.ResponseWriter) string {
+	buff := new(bytes.Buffer)
+	json.Indent(buff, []byte(claims), "", "  ")
+	jwt, err := buff.ReadString('}')
+	if err != nil {
+		log.Print(err)
+		http.Error(w, fmt.Sprintf("Failed to transribe claims into string"), http.StatusInternalServerError)
+		return ""
+	}
+	return jwt
+}
+
 func (app *serverApp) callbackHandler(w http.ResponseWriter, r *http.Request) {
 	var (
 		err   error
@@ -55,13 +77,16 @@ func (app *serverApp) callbackHandler(w http.ResponseWriter, r *http.Request) {
 	contxt := oidc.ClientContext(r.Context(), app.client)
 
 	oauth2Config := app.oauth2Config(nil)
-	authCode := r.FormValue("code")
-
-	log.Print(r.FormValue("state"))
+	authCode := getField(r, "code")
+	log.Print(authCode)
+	port := getField(r, "state")
+	if authCode == "" || port == "" {
+		http.Error(w, "400: Bad Request", http.StatusBadRequest)
+	}
 
 	token, err = oauth2Config.Exchange(contxt, authCode)
-
 	if err != nil {
+		log.Print(err)
 		http.Error(w, fmt.Sprintf("failed to get token: %v", err), http.StatusInternalServerError)
 		return
 	}
@@ -78,20 +103,33 @@ func (app *serverApp) callbackHandler(w http.ResponseWriter, r *http.Request) {
 		log.Print(err)
 		return
 	}
+
 	var claims json.RawMessage
 	if claimErr := idToken.Claims(&claims); err != nil {
 		log.Print(claimErr)
+		http.Error(w, fmt.Sprintf("Failed to get claims from JWT"), http.StatusInternalServerError)
+		return
 	}
-	buff := new(bytes.Buffer)
-	json.Indent(buff, []byte(claims), "", "  ")
-	jwt, err := buff.ReadString('}')
-	log.Print(jwt, err)
+
+	jwt := jwtToString(claims, w)
+
+	log.Print(jwt)
+	log.Print(port)
+
+	//sendBack(w, r, jwt, port)
 }
 
-func postTokenToCliHandler(jwtToken, port string) error {
+func sendBack(w http.ResponseWriter, r *http.Request, jwt string, port string) {
 	//will be easier to implement with a base cli created to communicate with this server
-	fmt.Print("send back to the client")
-	return nil
+	form := url.Values{}
+	form.Add("jwt", jwt)
+	url := "localhost:" + port
+	resp, err := http.Post(url, "application/x-www-form-encoded", strings.NewReader(form.Encode()))
+	if resp.StatusCode != 200 || err != nil {
+		http.Error(w, "Couldnt post to url: ["+url+"]", http.StatusBadRequest)
+		return
+	}
+	http.Redirect(w, r, url, http.StatusSeeOther)
 }
 
 func main() {
@@ -109,15 +147,6 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err.Error())
 	}
-	var claim struct {
-		User   string `json:"user"`
-		Groups string `json:"groups"`
-	}
-	if err := provider.Claims(&claim); err != nil {
-		log.Print(err)
-		return
-	}
-
 	app.provider = provider
 	app.verifier = provider.Verifier(&oidc.Config{ClientID: app.clientID})
 	mux := http.NewServeMux()
--- server/kubeloginserver_test.go
+++ server/kubeloginserver_test.go
@@ -9,36 +9,12 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
-	"strings"
 	"testing"
 )
 
-func dummyVerify(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body)
-	value, _ := url.ParseQuery(string(body))
-	if value.Get("jwt") != "" {
-		fmt.Fprint(w, "valid jwt")
-	} else {
-		fmt.Fprint(w, "not a valid jwt")
-	}
-}
-
-func dummyExchange(w http.ResponseWriter, r *http.Request) {
-	//r.ParseForm()
-	body, _ := ioutil.ReadAll(r.Body)
-	values, _ := url.ParseQuery(string(body))
-	if values.Get("clientID") != "" && values.Get("clientSecret") != "" && values.Get("authCode") != "" {
-		fmt.Fprint(w, "{id_token: myawesomeJWT}")
-	} else {
-		fmt.Fprint(w, "Error! Error!")
-	}
-
-}
-
 func callbackItems(w http.ResponseWriter, r *http.Request) {
-	authCode := r.FormValue("code")
-	state := r.FormValue("state")
+	authCode := getField(r, "code")
+	state := getField(r, "state")
 	returnedItems := authCode + ", " + state
 	fmt.Fprint(w, returnedItems)
 }
@@ -64,8 +40,7 @@ func TestSpecs(t *testing.T) {
 		incorrectPathServer := httptest.NewServer(http.HandlerFunc(incorrectURL))
 		cliGetTestServer := httptest.NewServer(http.HandlerFunc(app.handleCliLogin))
 		callbackItemsTestServer := httptest.NewServer(http.HandlerFunc(callbackItems))
-		exchangeTestServer := httptest.NewServer(http.HandlerFunc(dummyExchange))
-		verifyTestServer := httptest.NewServer(http.HandlerFunc(dummyVerify))
+
 		Convey("The incorrectURL handler should return a 404 if a user doesn't specify a path", func() {
 			response, _ := http.Get(incorrectPathServer.URL)
 			response.Body.Close()
@@ -92,61 +67,29 @@ func TestSpecs(t *testing.T) {
 			So(resp.StatusCode, ShouldEqual, 400)
 		})
 
-		Convey("Upon logging into auth page, callbackItems should return the authCode and state given in the url", func() {
-			url := callbackItemsTestServer.URL + "/callback?code=myawesomecode&state=myawesomestate"
+		Convey("getFields should read correct field values", func() {
+			url := callbackItemsTestServer.URL + "/callback?code=myawesomecode&state=8000"
 			resp, _ := http.Get(url)
 			responseData, _ := ioutil.ReadAll(resp.Body)
 			resp.Body.Close()
 			bodyString := string(responseData)
-			So(bodyString, ShouldEqual, "myawesomecode, myawesomestate")
+			So(bodyString, ShouldEqual, "myawesomecode, 8000")
 		})
-
-		Convey("After the authCode callback, should exchange authCode and other items to get a token with an id_token field containing a raw jwt", func() {
-			form := url.Values{}
-			form.Add("clientID", app.clientID)
-			form.Add("clientSecret", app.clientSecret)
-			form.Add("authCode", "myawesomeauthcode")
-			resp, _ := http.Post(exchangeTestServer.URL, "application/x-www-form-encoded", strings.NewReader(form.Encode()))
+		Convey("getFields should fail to read missing code field", func() {
+			url := callbackItemsTestServer.URL + "/callback?code=&state=8000"
+			resp, _ := http.Get(url)
 			responseData, _ := ioutil.ReadAll(resp.Body)
 			resp.Body.Close()
 			bodyString := string(responseData)
-			So(bodyString, ShouldEqual, "{id_token: myawesomeJWT}")
+			So(bodyString, ShouldEqual, ", 8000")
 		})
-
-		Convey("After the authCode callback, should exchange authCode and other items to get a token with an id_token field containing a raw jwt unless a value is missing", func() {
-			form := url.Values{}
-			form.Add("clientID", app.clientID)
-			form.Add("authCode", app.clientSecret)
-			resp, _ := http.Post(exchangeTestServer.URL, "application/x-www-form-encoded", strings.NewReader(form.Encode()))
+		Convey("getFields should fail to read missing state field", func() {
+			url := callbackItemsTestServer.URL + "/callback?code=myawesomecode&state="
+			resp, _ := http.Get(url)
 			responseData, _ := ioutil.ReadAll(resp.Body)
 			resp.Body.Close()
 			bodyString := string(responseData)
-			So(bodyString, ShouldEqual, "Error! Error!")
-		})
-
-		Convey("After getting the jwt, it should be verified and be valid", func() {
-			form := url.Values{}
-			form.Add("jwt", "myawesomejwt")
-			resp, _ := http.Post(verifyTestServer.URL, "application/x-www-form-encoded", strings.NewReader(form.Encode()))
-			responseData, _ := ioutil.ReadAll(resp.Body)
-			resp.Body.Close()
-			bodyString := string(responseData)
-			So(bodyString, ShouldEqual, "valid jwt")
-		})
-
-		Convey("After getting the jwt, if it's not valid then an error occurs", func() {
-			form := url.Values{}
-			form.Add("jwt", "")
-			resp, _ := http.Post(verifyTestServer.URL, "application/x-www-form-encoded", strings.NewReader(form.Encode()))
-			responseData, _ := ioutil.ReadAll(resp.Body)
-			resp.Body.Close()
-			bodyString := string(responseData)
-			So(bodyString, ShouldEqual, "not a valid jwt")
-		})
-
-		Convey("Server should finally redirect JWT to CLI at CLI's local port", func() {
-			response := postTokenToCliHandler("jwtToken", "8000")
-			So(response, ShouldEqual, nil)
+			So(bodyString, ShouldEqual, "myawesomecode, ")
 		})
 	})
 }
