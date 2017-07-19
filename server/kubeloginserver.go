c37efa827220144301fa354252c9e1279a0f2147
Ashish Amarnath <ashish.amarnath@nordstrom.com>
2017-07-18 22:15:43 -0700

    Convert all tabs to spaces

--- server/kubeloginserver.go
+++ server/kubeloginserver.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strings"
+    "bytes"
+    "context"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strings"
 
-	"github.com/coreos/go-oidc"
-	"golang.org/x/oauth2"
+    "github.com/coreos/go-oidc"
+    "golang.org/x/oauth2"
 )
 
 /*
    struct that contains necessary oauth/oidc information
 */
 type authOClient struct {
-	clientID     string
-	clientSecret string
-	redirectURI  string
-	verifier     *oidc.IDTokenVerifier
-	provider     *oidc.Provider
-	client       *http.Client
-	usernameSpec string
+    clientID     string
+    clientSecret string
+    redirectURI  string
+    verifier     *oidc.IDTokenVerifier
+    provider     *oidc.Provider
+    client       *http.Client
+    usernameSpec string
 }
 
 const (
-	idTokenField  = "id_token"
-	portField     = "port"
-	stateField    = "state"
-	groupsField   = "groups"
-	usernameField = "username"
-	authCodeField = "code"
+    idTokenField  = "id_token"
+    portField     = "port"
+    stateField    = "state"
+    groupsField   = "groups"
+    usernameField = "username"
+    authCodeField = "code"
 )
 
 /*
    the config for oauth2, scopes contain info we want back from the auth server
 */
 func (authClient *authOClient) getOAuth2Config(scopes []string) *oauth2.Config {
-	return &oauth2.Config{
-		ClientID:     authClient.clientID,
-		ClientSecret: authClient.clientSecret,
-		Endpoint:     authClient.provider.Endpoint(),
-		Scopes:       scopes,
-		RedirectURL:  authClient.redirectURI,
-	}
+    return &oauth2.Config{
+        ClientID:     authClient.clientID,
+        ClientSecret: authClient.clientSecret,
+        Endpoint:     authClient.provider.Endpoint(),
+        Scopes:       scopes,
+        RedirectURL:  authClient.redirectURI,
+    }
 }
 
 /*
@@ -55,11 +55,11 @@ func (authClient *authOClient) getOAuth2Config(scopes []string) *oauth2.Config {
 */
 func getField(request *http.Request, fieldName string) string {
 
-	if request.FormValue(fieldName) != "" {
-		log.Print("fieldname: [" + request.FormValue(fieldName) + "]")
-		return request.FormValue(fieldName)
-	}
-	return ""
+    if request.FormValue(fieldName) != "" {
+        log.Print("fieldname: [" + request.FormValue(fieldName) + "]")
+        return request.FormValue(fieldName)
+    }
+    return ""
 }
 
 /*
@@ -67,15 +67,15 @@ func getField(request *http.Request, fieldName string) string {
 */
 func rawMessageToString(claims json.RawMessage) string {
 
-	buff := new(bytes.Buffer)
-	json.Indent(buff, []byte(claims), "", "  ")
-	jwt, err := buff.ReadString('}')
-	if err != nil {
-		log.Print(err)
-		return err.Error()
-	}
-	log.Print(jwt)
-	return jwt
+    buff := new(bytes.Buffer)
+    json.Indent(buff, []byte(claims), "", "  ")
+    jwt, err := buff.ReadString('}')
+    if err != nil {
+        log.Print(err)
+        return err.Error()
+    }
+    log.Print(jwt)
+    return jwt
 }
 
 /*
@@ -83,14 +83,14 @@ func rawMessageToString(claims json.RawMessage) string {
 */
 func verifyJWT(jwt, usernameSpec string) bool {
 
-	groups := strings.Contains(jwt, groupsField)
-	username := strings.Contains(jwt, usernameField)
-	validUsername := strings.Contains(jwt, usernameSpec)
-	log.Print(groups, validUsername, username)
-	if groups && username && validUsername {
-		return true
-	}
-	return false
+    groups := strings.Contains(jwt, groupsField)
+    username := strings.Contains(jwt, usernameField)
+    validUsername := strings.Contains(jwt, usernameSpec)
+    log.Print(groups, validUsername, username)
+    if groups && username && validUsername {
+        return true
+    }
+    return false
 }
 
 /*
@@ -100,36 +100,36 @@ func verifyJWT(jwt, usernameSpec string) bool {
    we then redirect to the login page with the necessary info.
 */
 func (authClient *authOClient) handleCliLogin(writer http.ResponseWriter, request *http.Request) {
-	portState := request.FormValue(portField)
-	if portState == "" {
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-	var scopes = []string{"openid", " https://claims.nordstrom.com/nauth/username ", " https://claims.nordstrom.com/nauth/groups "}
-	authCodeURL := authClient.getOAuth2Config(scopes).AuthCodeURL(portState)
-	http.Redirect(writer, request, authCodeURL, http.StatusSeeOther)
+    portState := request.FormValue(portField)
+    if portState == "" {
+        http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+        return
+    }
+    var scopes = []string{"openid", " https://claims.nordstrom.com/nauth/username ", " https://claims.nordstrom.com/nauth/groups "}
+    authCodeURL := authClient.getOAuth2Config(scopes).AuthCodeURL(portState)
+    http.Redirect(writer, request, authCodeURL, http.StatusSeeOther)
 }
 
 func (authClient *authOClient) doAuthDance(requestContext context.Context, authCode string) (*oidc.IDToken, error) {
-	var (
-		err   error
-		token *oauth2.Token
-	)
-	nullToken := &oidc.IDToken{}
-	oidcClientContext := oidc.ClientContext(requestContext, authClient.client)
-	token, err = authClient.getOAuth2Config(nil).Exchange(oidcClientContext, authCode)
-	if err != nil {
-		log.Print("Failed to exchange token")
-		log.Print("Error: " + err.Error() + "\n" + authCode)
-		return nullToken, err
-	}
+    var (
+        err   error
+        token *oauth2.Token
+    )
+    nullToken := &oidc.IDToken{}
+    oidcClientContext := oidc.ClientContext(requestContext, authClient.client)
+    token, err = authClient.getOAuth2Config(nil).Exchange(oidcClientContext, authCode)
+    if err != nil {
+        log.Print("Failed to exchange token")
+        log.Print("Error: " + err.Error() + "\n" + authCode)
+        return nullToken, err
+    }
 
-	rawIDToken, ok := token.Extra(idTokenField).(string)
-	if !ok {
-		log.Print("Failed to get the id_token field")
-		return nullToken, err
-	}
-	return authClient.verifier.Verify(requestContext, rawIDToken)
+    rawIDToken, ok := token.Extra(idTokenField).(string)
+    if !ok {
+        log.Print("Failed to get the id_token field")
+        return nullToken, err
+    }
+    return authClient.verifier.Verify(requestContext, rawIDToken)
 }
 
 /*
@@ -137,64 +137,64 @@ func (authClient *authOClient) doAuthDance(requestContext context.Context, authC
    field that has the JWT. Upon verification of the jwt, we pull the claims out which is the info that is needed to send back to the client
 */
 func (authClient *authOClient) callbackHandler(writer http.ResponseWriter, request *http.Request) {
-	authCode := getField(request, authCodeField)
-	port := getField(request, stateField)
-	if authCode == "" || port == "" {
-		log.Print("Error! Need authcode and port. Received: " + authCode + " " + port)
-		http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-	idToken, err := authClient.doAuthDance(request.Context(), authCode)
-	if err != nil {
-		log.Print("Error verifying idToken: " + err.Error())
-		http.Error(writer, fmt.Sprintf("Failed to verify ID token"), http.StatusInternalServerError)
-		return
-	}
+    authCode := getField(request, authCodeField)
+    port := getField(request, stateField)
+    if authCode == "" || port == "" {
+        log.Print("Error! Need authcode and port. Received: " + authCode + " " + port)
+        http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+        return
+    }
+    idToken, err := authClient.doAuthDance(request.Context(), authCode)
+    if err != nil {
+        log.Print("Error verifying idToken: " + err.Error())
+        http.Error(writer, fmt.Sprintf("Failed to verify ID token"), http.StatusInternalServerError)
+        return
+    }
 
-	sendBackURL, err := generateSendBackURL(idToken, port, authClient.usernameSpec)
-	if err != nil {
-		http.Error(writer, "Failed to generate send back url", http.StatusInternalServerError)
-	}
-	http.Redirect(writer, request, sendBackURL, http.StatusSeeOther)
-	return
+    sendBackURL, err := generateSendBackURL(idToken, port, authClient.usernameSpec)
+    if err != nil {
+        http.Error(writer, "Failed to generate send back url", http.StatusInternalServerError)
+    }
+    http.Redirect(writer, request, sendBackURL, http.StatusSeeOther)
+    return
 }
 
 /*
    this will take the jwt and port and generate the url that will be redirected to
 */
 func generateSendBackURL(rawJWT *oidc.IDToken, port string, usernameSpec string) (string, error) {
-	var claims json.RawMessage
-	if claimErr := rawJWT.Claims(&claims); claimErr != nil {
-		log.Print("Error getting claims from idToken: " + claimErr.Error())
-		return "", claimErr
-	}
-	jwt := rawMessageToString(claims)
-	if !verifyJWT(jwt, usernameSpec) {
-		log.Print("Error! Failed to verify jwt: " + jwt)
-		return "", errors.New("JWT failed to verify")
-	}
-	sendBackURL := "http://localhost:" + port + "/client?jwt=" + jwt
-	return sendBackURL, nil
+    var claims json.RawMessage
+    if claimErr := rawJWT.Claims(&claims); claimErr != nil {
+        log.Print("Error getting claims from idToken: " + claimErr.Error())
+        return "", claimErr
+    }
+    jwt := rawMessageToString(claims)
+    if !verifyJWT(jwt, usernameSpec) {
+        log.Print("Error! Failed to verify jwt: " + jwt)
+        return "", errors.New("JWT failed to verify")
+    }
+    sendBackURL := "http://localhost:" + port + "/client?jwt=" + jwt
+    return sendBackURL, nil
 }
 
 //sets up the struct for later use
 func newAuthClient(clientID string, clientSecret string, redirectURI string, usernameSpec string, provider *oidc.Provider) authOClient {
-	var authClient authOClient
-	authClient.clientID = clientID
-	authClient.clientSecret = clientSecret
-	authClient.redirectURI = redirectURI
-	authClient.client = http.DefaultClient
-	authClient.provider = provider
-	authClient.verifier = provider.Verifier(&oidc.Config{ClientID: authClient.clientID})
-	authClient.usernameSpec = usernameSpec
-	return authClient
+    var authClient authOClient
+    authClient.clientID = clientID
+    authClient.clientSecret = clientSecret
+    authClient.redirectURI = redirectURI
+    authClient.client = http.DefaultClient
+    authClient.provider = provider
+    authClient.verifier = provider.Verifier(&oidc.Config{ClientID: authClient.clientID})
+    authClient.usernameSpec = usernameSpec
+    return authClient
 }
 
 func getMux(authClient authOClient) *http.ServeMux {
-	newMux := http.NewServeMux()
-	newMux.HandleFunc("/callback", authClient.callbackHandler)
-	newMux.HandleFunc("/login", authClient.handleCliLogin)
-	return newMux
+    newMux := http.NewServeMux()
+    newMux.HandleFunc("/callback", authClient.callbackHandler)
+    newMux.HandleFunc("/login", authClient.handleCliLogin)
+    return newMux
 }
 
 /*
@@ -203,12 +203,12 @@ func getMux(authClient authOClient) *http.ServeMux {
    the struct to contain necessary information
 */
 func main() {
-	contxt := oidc.ClientContext(context.Background(), http.DefaultClient)
-	provider, err := oidc.NewProvider(contxt, os.Getenv("OIDC_PROVIDER"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err.Error())
-	}
-	if err := http.ListenAndServe(os.Getenv("LISTEN_PORT"), getMux(newAuthClient(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SEC"), os.Getenv("REDIRECT"), os.Getenv("USERNAME_SPEC"), provider))); err != nil {
-		log.Fatal(err)
-	}
+    contxt := oidc.ClientContext(context.Background(), http.DefaultClient)
+    provider, err := oidc.NewProvider(contxt, os.Getenv("OIDC_PROVIDER"))
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "error: %v\n", err.Error())
+    }
+    if err := http.ListenAndServe(os.Getenv("LISTEN_PORT"), getMux(newAuthClient(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SEC"), os.Getenv("REDIRECT"), os.Getenv("USERNAME_SPEC"), provider))); err != nil {
+        log.Fatal(err)
+    }
 }
--- server/kubeloginserver_test.go
+++ server/kubeloginserver_test.go
@@ -1,142 +1,142 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"net/http/httptest"
-	"testing"
+    "log"
+    "net/http"
+    "net/http/httptest"
+    "testing"
 
-	"github.com/coreos/go-oidc"
-	. "github.com/smartystreets/goconvey/convey"
+    "github.com/coreos/go-oidc"
+    . "github.com/smartystreets/goconvey/convey"
 )
 
 func incorrectURL(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "404 Page Not Found", http.StatusNotFound)
+    http.Error(w, "404 Page Not Found", http.StatusNotFound)
 }
 
 func TestServerSpecs(t *testing.T) {
-	Convey("Kubelogin Server", t, func() {
-		provider := &oidc.Provider{}
-		authClient := newAuthClient("foo", "bar", "redirect", "userSpec", provider)
-		unitTestServer := httptest.NewServer(getMux(authClient))
-		Convey("The incorrectURL handler should return a 404 if a user doesn't specify a path", func() {
-			response, _ := http.Get(unitTestServer.URL)
-			response.Body.Close()
-			So(response.StatusCode, ShouldEqual, 404)
-		})
-		Convey("The cliHandleLogin function", func() {
-			Convey("should get a status code 303 for a correct redirect", func() {
-				url := unitTestServer.URL + "/login?port=8000"
-				authClient.client = &http.Client{
-					CheckRedirect: func(req *http.Request, via []*http.Request) error {
-						return http.ErrUseLastResponse
-					},
-				}
-				request, _ := http.NewRequest("GET", url, nil)
-				resp, _ := authClient.client.Do(request)
-				log.Print(resp.StatusCode)
-				resp.Body.Close()
-				So(resp.StatusCode, ShouldEqual, 303)
-			})
-			Convey("should return a 400 error if the port is missing", func() {
-				url := unitTestServer.URL + "/login?port="
-				resp, _ := http.Get(url)
-				resp.Body.Close()
-				So(resp.StatusCode, ShouldEqual, 400)
-			})
-		})
-		Convey("callbackHandler", func() {
-			Convey("should return a bad request if no code or state is in the url", func() {
-				url := unitTestServer.URL + "/callback"
-				request, _ := http.NewRequest("GET", url, nil)
-				response, _ := authClient.client.Do(request)
-				response.Body.Close()
-				So(response.StatusCode, ShouldEqual, http.StatusBadRequest)
-			})
-			Convey("should return a internal server error if the authcode is not valid", func() {
-				fakeCodeURL := unitTestServer.URL + "/callback?code=asdf123&state=3000"
-				request, _ := http.NewRequest("GET", fakeCodeURL, nil)
-				response, _ := authClient.client.Do(request)
-				response.Body.Close()
-				So(response.StatusCode, ShouldEqual, http.StatusInternalServerError)
-			})
-		})
-	})
+    Convey("Kubelogin Server", t, func() {
+        provider := &oidc.Provider{}
+        authClient := newAuthClient("foo", "bar", "redirect", "userSpec", provider)
+        unitTestServer := httptest.NewServer(getMux(authClient))
+        Convey("The incorrectURL handler should return a 404 if a user doesn't specify a path", func() {
+            response, _ := http.Get(unitTestServer.URL)
+            response.Body.Close()
+            So(response.StatusCode, ShouldEqual, 404)
+        })
+        Convey("The cliHandleLogin function", func() {
+            Convey("should get a status code 303 for a correct redirect", func() {
+                url := unitTestServer.URL + "/login?port=8000"
+                authClient.client = &http.Client{
+                    CheckRedirect: func(req *http.Request, via []*http.Request) error {
+                        return http.ErrUseLastResponse
+                    },
+                }
+                request, _ := http.NewRequest("GET", url, nil)
+                resp, _ := authClient.client.Do(request)
+                log.Print(resp.StatusCode)
+                resp.Body.Close()
+                So(resp.StatusCode, ShouldEqual, 303)
+            })
+            Convey("should return a 400 error if the port is missing", func() {
+                url := unitTestServer.URL + "/login?port="
+                resp, _ := http.Get(url)
+                resp.Body.Close()
+                So(resp.StatusCode, ShouldEqual, 400)
+            })
+        })
+        Convey("callbackHandler", func() {
+            Convey("should return a bad request if no code or state is in the url", func() {
+                url := unitTestServer.URL + "/callback"
+                request, _ := http.NewRequest("GET", url, nil)
+                response, _ := authClient.client.Do(request)
+                response.Body.Close()
+                So(response.StatusCode, ShouldEqual, http.StatusBadRequest)
+            })
+            Convey("should return a internal server error if the authcode is not valid", func() {
+                fakeCodeURL := unitTestServer.URL + "/callback?code=asdf123&state=3000"
+                request, _ := http.NewRequest("GET", fakeCodeURL, nil)
+                response, _ := authClient.client.Do(request)
+                response.Body.Close()
+                So(response.StatusCode, ShouldEqual, http.StatusInternalServerError)
+            })
+        })
+    })
 }
 
 func TestGetField(t *testing.T) {
-	Convey("getFields", t, func() {
-		url := "localhost/callback?code=myawesomecode&state=8000"
-		newReq, _ := http.NewRequest("GET", url, nil)
+    Convey("getFields", t, func() {
+        url := "localhost/callback?code=myawesomecode&state=8000"
+        newReq, _ := http.NewRequest("GET", url, nil)
 
-		Convey("returns the code field value from the url", func() {
-			result := getField(newReq, authCodeField)
-			So(result, ShouldEqual, "myawesomecode")
-		})
-		Convey("returns the state field value", func() {
-			result := getField(newReq, stateField)
-			So(result, ShouldEqual, "8000")
-		})
-		Convey("returns an empty string upon a missing field", func() {
-			result := getField(newReq, "helloworld")
-			So(result, ShouldEqual, "")
-		})
+        Convey("returns the code field value from the url", func() {
+            result := getField(newReq, authCodeField)
+            So(result, ShouldEqual, "myawesomecode")
+        })
+        Convey("returns the state field value", func() {
+            result := getField(newReq, stateField)
+            So(result, ShouldEqual, "8000")
+        })
+        Convey("returns an empty string upon a missing field", func() {
+            result := getField(newReq, "helloworld")
+            So(result, ShouldEqual, "")
+        })
 
-	})
+    })
 }
 
 func TestVerifyJWT(t *testing.T) {
-	Convey("verifyJwt", t, func() {
-		testJwt := "https://claims.nordstrom.com/nauth/groups, https://claims.nordstrom.com/nauth/username, @nordstrom.com"
-		testResult := verifyJWT(testJwt, "@nordstrom.com")
-		Convey("should return true upon the username, usernameSpec, and groups fields being present", func() {
-			So(testResult, ShouldEqual, true)
-		})
-		Convey("should return false if a field is missing", func() {
-			testResult = verifyJWT("nordy", "usernameSpec")
-			So(testResult, ShouldEqual, false)
-		})
-	})
+    Convey("verifyJwt", t, func() {
+        testJwt := "https://claims.nordstrom.com/nauth/groups, https://claims.nordstrom.com/nauth/username, @nordstrom.com"
+        testResult := verifyJWT(testJwt, "@nordstrom.com")
+        Convey("should return true upon the username, usernameSpec, and groups fields being present", func() {
+            So(testResult, ShouldEqual, true)
+        })
+        Convey("should return false if a field is missing", func() {
+            testResult = verifyJWT("nordy", "usernameSpec")
+            So(testResult, ShouldEqual, false)
+        })
+    })
 }
 
 func TestGenerateSendBackURL(t *testing.T) {
-	Convey("generateSendBackURL", t, func() {
-		Convey("should generate an error due to the token not containing a claims field", func() {
-			nullToken := &oidc.IDToken{}
-			testSendBackURL, err := generateSendBackURL(nullToken, "3000", "usernameSpec")
-			log.Print(err)
-			So(testSendBackURL, ShouldEqual, "")
-		})
-	})
+    Convey("generateSendBackURL", t, func() {
+        Convey("should generate an error due to the token not containing a claims field", func() {
+            nullToken := &oidc.IDToken{}
+            testSendBackURL, err := generateSendBackURL(nullToken, "3000", "usernameSpec")
+            log.Print(err)
+            So(testSendBackURL, ShouldEqual, "")
+        })
+    })
 }
 
 func TestRawMessageToString(t *testing.T) {
-	Convey("rawMessageToString", t, func() {
+    Convey("rawMessageToString", t, func() {
 
-		Convey("returns a string if a valid byte array is given", func() {
-			testJwt := rawMessageToString([]byte{123, 34, 97, 108, 103, 34, 58, 34, 82, 83, 50, 53, 54, 34, 125})
-			So(testJwt, ShouldContainSubstring, "alg")
-		})
-		Convey("returns an EOF because there is a missing } as the delimiting byte", func() {
-			failedJwt := rawMessageToString([]byte{123, 34, 97, 108, 103, 34, 58, 34, 82, 83, 50, 53, 54, 34})
-			So(failedJwt, ShouldEqual, "EOF")
-		})
-	})
+        Convey("returns a string if a valid byte array is given", func() {
+            testJwt := rawMessageToString([]byte{123, 34, 97, 108, 103, 34, 58, 34, 82, 83, 50, 53, 54, 34, 125})
+            So(testJwt, ShouldContainSubstring, "alg")
+        })
+        Convey("returns an EOF because there is a missing } as the delimiting byte", func() {
+            failedJwt := rawMessageToString([]byte{123, 34, 97, 108, 103, 34, 58, 34, 82, 83, 50, 53, 54, 34})
+            So(failedJwt, ShouldEqual, "EOF")
+        })
+    })
 }
 
 func TestNewAuthClient(t *testing.T) {
-	Convey("authClientSetup", t, func() {
-		provider := &oidc.Provider{}
-		Convey("authClientSetup should return a serverApp struct with the clientid/secret, redirect URL, and defaultClient info filled in", func() {
-			testClient := newAuthClient("foo", "bar", "redirect", "usernameSpec", provider)
-			correctID := testClient.clientID == "foo"
-			correctSec := testClient.clientSecret == "bar"
-			correctURI := testClient.redirectURI == "redirect"
-			correctClient := testClient.client == http.DefaultClient
-			correctSpec := testClient.usernameSpec == "usernameSpec"
+    Convey("authClientSetup", t, func() {
+        provider := &oidc.Provider{}
+        Convey("authClientSetup should return a serverApp struct with the clientid/secret, redirect URL, and defaultClient info filled in", func() {
+            testClient := newAuthClient("foo", "bar", "redirect", "usernameSpec", provider)
+            correctID := testClient.clientID == "foo"
+            correctSec := testClient.clientSecret == "bar"
+            correctURI := testClient.redirectURI == "redirect"
+            correctClient := testClient.client == http.DefaultClient
+            correctSpec := testClient.usernameSpec == "usernameSpec"
 
-			overallCorrect := correctClient && correctID && correctSec && correctURI && correctSpec
-			So(overallCorrect, ShouldEqual, true)
-		})
-	})
+            overallCorrect := correctClient && correctID && correctSec && correctURI && correctSpec
+            So(overallCorrect, ShouldEqual, true)
+        })
+    })
 }
